d298a73bbc44885c61803127373f8cc9d2a8b1ae
questionmarkexclamationpoint <anna.c.commons@gmail.com>
2019-01-13 05:18:58 -0800

    replace history

--- lib/shitpost_bot/events/message.rb
+++ lib/shitpost_bot/events/message.rb
@@ -5,7 +5,7 @@ module ShitpostBot
       message do |event|
         unless event.content.empty? || event.content[0] == BOT.prefix || rand >= event.channel.reply.to_f || (event.channel.mention && event.message.bot_mention?)
           event.channel.start_typing
-          start_text = Processing.format_messages(event.channel.recent_history, true)
+          start_text = Processing.format_messages(event.channel.history, true)
           start_text = start_text[-500..-1] if start_text.length > 500
           reply = Posting.get_reply(start_text, event.channel)
           reply.each do |r|
