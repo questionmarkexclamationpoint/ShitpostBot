4760d7e228c1d2f5c5199ed5f437ce22205f6258
questionmarkexclamationpoint <anna.c.commons@gmail.com>
2019-12-18 03:49:38 -0800

    log error if stderr not empty

--- lib/util.rb
+++ lib/util.rb
@@ -2,6 +2,7 @@ module Util
   def self.syscall(*cmd)
     begin
       stdout, stderr, status = Open3.capture3(*cmd)
+      puts "ERROR: #{stderr}" unless stderr.chomp.empty?
       stdout.slice!(0..-(1 + $/.size)) # strip trailing eol
     rescue => e
       puts e
