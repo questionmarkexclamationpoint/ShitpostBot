d0642872f5c388b151dadb2a8416d800d198ea17
questionmarkexclamationpoint <anna.c.commons@gmail.com>
2019-12-26 11:20:52 -0800

    fix mention event

--- lib/shitpost_bot/events/mention.rb
+++ lib/shitpost_bot/events/mention.rb
@@ -6,7 +6,7 @@ module ShitpostBot
         STATS.mentions += 1
         if event.channel.mention && event.content[0] != BOT.prefix
           event.channel.start_typing
-          start_text = Processing.format_messages(event.channel.history.reverse.each, with_bot: true).to_a
+          start_text = Processing.format_messages(event.channel.history.reverse.each, with_bot: true).to_a.join
           start_text = start_text[-500..-1] if start_text.length > 500
           reply = Posting.get_reply(start_text, event.channel)
           reply.each do |r|
