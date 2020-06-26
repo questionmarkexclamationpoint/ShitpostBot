e8890db92cbe35afc8f4857dc4ff140f38630763
questionmarkexclamationpoint <anna.c.commons@gmail.com>
2020-06-26 04:10:57 -0700

    remove author from about

--- lib/shitpost_bot/commands/about.rb
+++ lib/shitpost_bot/commands/about.rb
@@ -8,7 +8,6 @@ module ShitpostBot
               max_args: 0,
               help_available: true
               ) do |event|
-        event << 'Author: ?!#9874 (<@178950134263054337>)'
         event << "Owner: <@#{CONFIG.owner_id}>"
         event << 'Github: https://github.com/questionmarkexclamationpoint/ShitpostBot'
         event << "Version: #{VERSION}"
